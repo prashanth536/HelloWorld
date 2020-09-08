70c2da3415240591d7cb12c4387c0f9a0350b43b
Prashanth Polusani <ppolusan@its.jnj.com>
2020-09-08 14:58:06 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
